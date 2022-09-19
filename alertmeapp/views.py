54939cea5561b954ee166695c64109afe98a75ae
Sanket Kamta <iamsanketkamta@gmail.com>
2022-09-19 20:15:31 +0530

    Update views.py
    new connection made

--- alertmeapp/views.py
+++ alertmeapp/views.py
@@ -1,23 +1,25 @@
 #Login to Mongo Atlas using fascinatemeok@gmail.com
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from pymongo import MongoClient
 
 def homepage(request):
     return render(request, "index.html")
 
 def thankyou(request):
-    cluster = "mongodb+srv://sanket:xieViGwln0QF117U@cluster0.2webm.mongodb.net/AlerrtMeDB?retryWrites=true&w=majority"
-    client = MongoClient(cluster)
-    print("Connection created!!")
-    db = client.AlerrtMeDB
-    alertdata = db.alertdata
-    name = request.POST["name"]
-    age = request.POST["age"]
-    email = request.POST["email"]
-    url = request.POST["url"]
-    threshold = request.POST["threshold"]
-    inputdata = {"name":name, "age":age, "email":email, "url":url, "threshold":threshold}
-    alertdata.insert_one(inputdata)
-    print("Data inserted!!")
-    return render(request, "index.html")
+    try:
+        cluster = "mongodb+srv://sanket:0311@cluster0.ibkyza6.mongodb.net/?retryWrites=true&w=majority"
+
+        client = MongoClient(cluster)
+        db = client.AlerrtMeDB
+        alertdata = db.alertdata
+        name = request.POST["name"]
+        age = request.POST["age"]
+        email = request.POST["email"]
+        url = request.POST["url"]
+        threshold = request.POST["threshold"]
+        inputdata = {"name":name, "age":age, "email":email, "url":url, "threshold":threshold, "count":1}
+        alertdata.insert_one(inputdata)
+        return redirect('homepage')
+
+    except Exception as e:
+        print(e)
